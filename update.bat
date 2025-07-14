3a42b1a8fd64373c58b7a5a7db5d2c3e105bed3b
ArchorByte <archorbyte@gmail.com>
2025-07-14 10:19:23 +0200

    Release 1.4.1

--- update.bat
+++ update.bat
@@ -2,5 +2,4 @@
 cls
 pip install -t ./ pytubefix --upgrade
 pip install -t ./ requests --upgrade
-pip install -t ./ pycountry --upgrade
 pause
--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 clear
 pip install -t ./ pytubefix --upgrade
 pip install -t ./ requests --upgrade
-pip install -t ./ pycountry --upgrade
